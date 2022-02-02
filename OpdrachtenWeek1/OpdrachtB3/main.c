462b59d80b3a414df73549d6bfcf14786606a4ff
luuk <lah.vanberkel@student.avans.nl>
2022-02-02 12:36:57 +0100

    added 2 extra projects

---
+++ OpdrachtenWeek1/OpdrachtB3/main.c
@@ -0,0 +1,18 @@
+/*
+ * OpdrachtB3.c
+ *
+ * Created: 2/2/2022 12:35:50 PM
+ * Author : berke
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ OpdrachtenWeek1/OpdrachtB4/main.c
@@ -0,0 +1,18 @@
+/*
+ * OpdrachtB4.c
+ *
+ * Created: 2/2/2022 12:36:22 PM
+ * Author : berke
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
