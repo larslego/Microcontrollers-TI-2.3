34324957f8526cfada99336b209a9e0c2ff2a27c
luuk <lah.vanberkel@student.avans.nl>
2022-02-02 12:18:17 +0100

    added: project week 1

---
+++ OpdrachtenWeek1/OpdrachtenWeek1/main.c
@@ -0,0 +1,18 @@
+/*
+ * OpdrachtenWeek1.c
+ *
+ * Created: 2/2/2022 12:17:52 PM
+ * Author : berke
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
