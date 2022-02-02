7ba5dd279b6ebacc039e09c2b004de0c42b31750
luuk <lah.vanberkel@student.avans.nl>
2022-02-02 12:49:09 +0100

    fixed a project

--- OpdrachtenWeek1/OpdrachtB3/main.c
+++ OpdrachtenWeek1/OpdrachtB3/main.c
@@ -1,6 +1,17 @@
 /*
- * main.c
+ * OpdrachtB4.c
  *
- * Created: 2/2/2022 12:39:57 PM
+ * Created: 2/2/2022 12:40:26 PM
  * Author : berke
  */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
