762680ce82f96c174b341eeb827385525f86a0ce
luuk <lah.vanberkel@student.avans.nl>
2022-02-02 13:28:43 +0100

    Added: Opdracht B3

--- OpdrachtenWeek1/OpdrachtB3/main.c
+++ OpdrachtenWeek1/OpdrachtB3/main.c
@@ -1,17 +1,57 @@
-/*
- * OpdrachtB4.c
- *
- * Created: 2/2/2022 12:40:26 PM
- * Author : berke
- */ 
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** knipper.c
+**
+** Beschrijving:	Toggle even en oneven leds PORTD  
+** -------------------------------------------------------------------------*/
+
+#define F_CPU 8e6
 
 #include <avr/io.h>
+#include <util/delay.h>
 
 
-int main(void)
+/******************************************************************/
+void wait( int ms )
+/* 
+short:			Busy wait number of millisecs
+inputs:			int ms (Number of millisecs to busy wait)
+outputs:	
+notes:			Busy wait, not very accurate. Make sure (external)
+				clock value is set. This is used by _delay_ms inside
+				util/delay.h
+*******************************************************************/
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	for (int i=0; i<ms; i++) {
+		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
+	}
 }
+
+/******************************************************************/
+int main( void ) {
+/* 
+short:			main() loop, entry point of executable
+inputs:			
+outputs:	
+notes:			Looping forever, flipping bits on PORTD
+*******************************************************************/
+
+	DDRD = 0b11111111;			// All pins PORTD are set to output 
+	DDRC = 0b00000000;			// All pins PORTC are set to input 
+	
+	while (1) {
+		PORTD  =0x00;
+		if (PINC != 0x01){
+			PORTD  =0x00;
+			wait(1000);
+			PORTD =0x80;		
+			wait(1000);
+		}
+	}
+
+	return 1;
+}
+
