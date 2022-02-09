dc84070c77848d3cd3a46ef27db3cd2811b79f0b
luuk <lah.vanberkel@student.avans.nl>
2022-02-09 11:18:09 +0100

    Fix

--- OpdrachtenWeek2/OpdrachtB2/main.c
+++ OpdrachtenWeek2/OpdrachtB2/main.c
@@ -5,11 +5,37 @@
  * Author : berke
  */ 
 
+#define F_CPU 8e6
 #include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int i = 0;
+
+ISR( INT0_vect ) {
+	i--;
+	i = (i < 0 ? 7 : i);
+	PORTC = (1<<i);
+}
+
+
+ISR( INT1_vect ) {
+	i++;
+	i = (i > 7 ? 0 : i);
+	PORTC = (1<<i);
+}
 
 
 int main(void)
 {
+	DDRC = 0xFF; //Setting row to output
+	DDRD = 0x00;
+	
+	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
+	EIMSK |= 0x03;			// Enable INT1 & INT0	
+
+	sei();	
+	
     /* Replace with your application code */
     while (1) 
     {
